c22e0f902edec1a27d7ac2dd8c42ec8ca60b92f9
adhammohamed01 <adham.mhamed2001@gmail.com>
2023-12-23 01:50:09 +0200

    adding some edits

--- src/InterruptController_tb.v
+++ src/InterruptController_tb.v
@@ -1,197 +1,75 @@
 module InterruptController_tb ();
     
-reg INTA, IR0, IR1, IR2, IR3, IR4, IR5, IR6, IR7, A0, RD, WR, CS;
-wire INT;
-wire [7:0] DATABUS;
+    reg INTA, IR0, IR1, IR2, IR3, IR4, IR5, IR6, IR7, A0, RD, WR, CS_master, CS_slave, SP_EN_master, SP_EN_slave;
+    wire INT_master, INT_slave;
+    wire [2:0] CAS;
+    wire [7:0] DATABUS;
 
-reg [7:0] inout_drive;
-wire [7:0] inout_recv;
+    reg [7:0] databus_drive;
+    reg [8:0] commands [7:0];
+    integer i = 0;
 
-assign DATABUS = inout_drive;
-assign inout_recv = DATABUS;
+    assign DATABUS = databus_drive;
 
-InterruptController InterruptController (INTA, INT, IR0, IR1, IR2, IR3, IR4, IR5, IR6, IR7, RD, WR, A0, CS, DATABUS);
+    InterruptController InterruptControllerMaster (INTA, INT_master, IR0, INT_slave, IR2, IR3, IR4, IR5, IR6, IR7, RD, WR, A0, CS_master, DATABUS, CAS, SP_EN_master);
+    InterruptController InterruptControllerSlave (INTA, INT_slave, IR0, IR1, IR2, IR3, IR4, IR5, IR6, IR7, RD, WR, A0, CS_slave, DATABUS, CAS, SP_EN_slave);
 
-initial begin
+    initial begin
+        SP_EN_master = 1;
+        SP_EN_slave = 0;
+        WR = 1;
+        RD = 1;
+        INTA = 1;
+        
+        $readmemb("commands.txt", commands);
 
-    IR0 = 0;
-    IR1 = 0;
-    IR2 = 0;
-    IR3 = 0;
-    IR4 = 0;
-    IR5 = 0;
-    IR6 = 0;
-    IR7 = 0;
-    INTA = 1;
-    WR = 1;
-    RD = 1;
-    CS = 0;
-    #5
+        CS_slave = 1;
+        CS_master = 0;
+        for (i = 0; i <= 3; i = i + 1) begin // To initialize master
+            A0 = commands[i][8];
+            databus_drive = commands[i][7:0];
+            #5
+            WR = 0;
+            #5
+            WR = 1;
+        end
 
-    inout_drive = 8'b10111101;
-    A0 = 0;
-    #1
-    WR = 0;
-    #5
-    WR = 1;
-    #5    
+        CS_slave = 0;
+        CS_master = 1;
+        for (i = 4; i <= 7; i = i + 1) begin // To initialize slave
+            A0 = commands[i][8];
+            databus_drive = commands[i][7:0];
+            #5
+            WR = 0;
+            #5
+            WR = 1;
+        end
+        databus_drive = 8'bZZZZZZZZ;
 
-    inout_drive = 8'b10111100;
-    A0 = 1;
-    #1
-    WR = 0;
-    #5
-    WR = 1;
-    #5
+        #5
 
-    inout_drive = 8'b00000000;
-    #1
-    WR = 0;
-    #5
-    WR = 1;
-    #5
-
-    inout_drive = 8'b00000010;
-    #1
-    WR = 0;
-    #5
-    inout_drive = 8'hZZ;
-    WR = 1;
-    #5
-
-    inout_drive = 8'b00111100;
-    A0 = 1;
-    #1
-    WR = 0;
-    #5
-    A0 = 0;
-    inout_drive = 8'hZZ;
-    WR = 1;
-    #5
-
-    inout_drive = 8'b11100000;
-    A0 = 0;
-    #1
-    WR = 0;
-    #5
-    inout_drive = 8'hZZ;
-    WR = 1;
-    #5
-
-    inout_drive = 8'b00001000;
-    A0 = 0;
-    #1
-    WR = 0;
-    #5
-    inout_drive = 8'hZZ;
-    WR = 1;
-    #5
-
-    inout_drive = 8'b10111110;
-    A0 = 0;
-    #1
-    WR = 0;
-    #5
-    WR = 1;
-    #5
-
-    inout_drive = 8'b10101110;
-    A0 = 1;
-    #1
-    WR = 0;
-    #5
-    inout_drive = 8'hZZ;
-    WR = 1;
-    #5
-
-    inout_drive = 8'b10100110;
-    A0 = 0;
-    #1
-    WR = 0;
-    #5
-    inout_drive = 8'hZZ;
-    WR = 1;
-
-    #10
-
-    IR3 = 1;
-    #3
-    IR1 = 1;
-    #7
-
-    INTA = 0;
-    #5
-    INTA = 1;
-    #5
-    INTA = 0;
-    RD = 0;
-    #5
-    INTA = 1;
-    RD = 1;
-    IR1 = 0;
-    #10
-
-    INTA = 0;
-    #5
-    INTA = 1;
-    #5
-    INTA = 0;
-    RD = 0;
-    #5
-    INTA = 1;
-    RD = 1;
-    IR3 = 0;
-    #5
-
-    $stop;
-
-end
-
+        IR0 = 0;
+        IR1 = 1; // do drive the input intrrupt of slave
+        IR2 = 0;
+        IR3 = 0;
+        IR4 = 0;
+        IR5 = 0;
+        IR6 = 0;
+        IR7 = 0;
+        #5
+        INTA = 1;
+        #5
+        INTA = 0;
+        #5
+        INTA = 1;
+        #5
+        INTA = 0;
+        #5
+        INTA = 1;
+        IR1 = 0;
+        #5
+        
+        $stop;
+    end
 
 endmodule
-
-/*
-    WR=1;
-    #5
-    WR=0;
-    #5
-    A0 = 0;
-    inout_drive=8'b10111110;
-    #5
-    WR=1;
-    #5
-    WR=0;
-    #5
-    inout_drive=8'b10111100;
-    #5
-    WR=1;
-    #5
-    IR0 = 0;
-
-    #5;
-
-    IR0 = 1;
-    IR1 = 0;
-    IR2 = 0;
-    IR3 = 0;
-    IR4 = 0;
-    IR5 = 0;
-    IR6 = 0;
-    IR7 = 0;
-    INTA = 1;
-
-    #10;
-
-
-    INTA = 0;
-    #5;
-    INTA = 1;
-    #5;
-    INTA = 0;
-    #5;
-    INTA = 1;
-
-    IR0 = 0;    
-
-    $stop;
-*/
