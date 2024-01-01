9c55e60f6ada23064ccbe166cd90a49317e0bab4
Ahmed <ahmedsherif602@gmail.com>
2024-01-02 01:09:28 +0200

    Cascade was added and DataBuffer is Updated

---
+++ src/Cascade.v
@@ -0,0 +1,63 @@
+module Cascade (CAS, SP_EN, isr_highest_bit, icw3, send_vector_address);
+    
+    inout [2:0] CAS;
+    input SP_EN;
+    input [7:0] isr_highest_bit, icw3;
+    output reg send_vector_address;
+
+    wire [2:0] cas_read;
+    reg [2:0] cas_write;
+
+    assign CAS = SP_EN ? cas_write : 3'hZ;
+    assign cas_read = CAS;
+
+    always @(*) begin
+        if (SP_EN) begin
+            case (icw3 & isr_highest_bit)
+                8'h01: begin
+                    cas_write <= 3'b000;
+                    send_vector_address <= 0;
+                end
+                8'h02: begin
+                    cas_write <= 3'b001;
+                    send_vector_address <= 0;
+                end
+                8'h04: begin
+                    cas_write <= 3'b010;
+                    send_vector_address <= 0;
+                end
+                8'h08: begin
+                    cas_write <= 3'b011;
+                    send_vector_address <= 0;
+                end
+                8'h10: begin
+                    cas_write <= 3'b100;
+                    send_vector_address <= 0;
+                end
+                8'h20: begin
+                    cas_write <= 3'b101;
+                    send_vector_address <= 0;
+                end
+                8'h40: begin
+                    cas_write <= 3'b110;
+                    send_vector_address <= 0;
+                end
+                8'h80: begin
+                    cas_write <= 3'b111;
+                    send_vector_address <= 0;
+                end
+                default: begin
+                    cas_write <= 3'b000;
+                    send_vector_address <= 1;
+                end
+            endcase
+        end
+        else begin // slave mode
+            if (cas_read == icw3[2:0]) // check if cascasde is same as my id
+                send_vector_address <= 1;
+            else
+                send_vector_address <= 0;
+        end    
+    end
+    
+endmodule
--- src/DataBusBuffer.v
+++ src/DataBusBuffer.v
@@ -1,11 +1,11 @@
-module DataBusBuffer (DATABUS, direction, command_word, vector_address);
+module DataBusBuffer (DATABUS, direction, databus_output, command_word);
 
-    inout [7:0] DATABUS; //internal bus when send ** output **  or bus from pc when read so **input** 
-    input direction;  //1>>sending to pc
-    output [7:0] command_word; //recieved data
-    input [7:0] vector_address; //sent data
+    inout [7:0] DATABUS;
+    input direction;
+    input [7:0] databus_output;
+    output [7:0] command_word;
 
-    assign DATABUS = direction ? vector_address : 8'hZZ;
+    assign DATABUS = direction ? databus_output : 8'hZZ;
     assign command_word = DATABUS;
 
 endmodule
